5e0a725ca4c6121bdc91bb3cfd774f36ac0abab5
luzi82 <luzi82@gmail.com>
2012-08-08 23:40:53 +0800

    force db path relative

--- owatube/settings.py
+++ owatube/settings.py
@@ -1,4 +1,7 @@
 # Django settings for owatube project.
+import os
+
+OWATUBE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,7 +15,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/luzi82/project/owt/workspace/owatube/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': OWATUBE_PATH+'/sqlite.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -109,6 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    OWATUBE_PATH+"/template"
 )
 
 INSTALLED_APPS = (
