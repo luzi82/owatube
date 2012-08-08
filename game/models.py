754e02efd17da0a3ba0058c8a48124b4a0ed2af7
luzi82 <luzi82@gmail.com>
2012-08-08 17:27:57 +0800

    bug fix, admin show items

--- game/models.py
+++ game/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth import models as auth_models
 
 class Game(models.Model):
-    author = auth_models.User
+    author = models.ForeignKey(auth_models.User)
+    create_date = models.DateTimeField()
