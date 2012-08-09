94ec8bb6a996fba83e817640ccbe0b59295e6ed6
luzi82 <luzi82@gmail.com>
2012-08-09 23:57:17 +0800

    some coding on member reg form

--- member/views.py
+++ member/views.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
 
 def register(request):
-    return HttpResponse("reg me")
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            HttpResponseRedirect(reverse("game.views.index"))
+    else:
+        form = RegisterForm()
+    return render(request,"register.html",{"form":form})
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length=20)
+    password = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput())
