a06086653cc08c1ed8aac16ea1d285519c6720ff
luzi82 <luzi82@gmail.com>
2012-08-13 07:47:29 +0800

    add test

--- member/tests.py
+++ member/tests.py
@@ -6,11 +6,20 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.test.client import Client
+import pprint
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+    def test_basic_reg(self):
+        c = Client()
+        response = c.post("/member/register/",{"username":"hellouser","password0":"hellopass","password1":"hellopass"},follow=True)
+        self.assertRedirects(response,"/member/register_success/")
+
+    def test_short_username(self):
+        c = Client()
+        response = c.post("/member/register/",{"username":"h","password0":"hellopass","password1":"hellopass"},follow=False)
+#        self.assertEqual(response.template, second, msg)
+        self.assertEqual(response.status_code, 200)
+#        pprint.pprint(response.template)
