e16e9c6648dea17276ccfe5765a8e65db2f86e36
luzi82 <luzi82@gmail.com>
2012-08-13 07:47:09 +0800

    add register success using template extends

--- member/urls.py
+++ member/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns("member.views",
     url(r'^register/$', 'register'),
+    url(r'^register_success/$', 'register_success'),
 )
--- member/views.py
+++ member/views.py
@@ -7,12 +7,26 @@ def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
-            HttpResponseRedirect(reverse("game.views.index"))
+            return HttpResponseRedirect(reverse("member.views.register_success"))
     else:
         form = RegisterForm()
-    return render(request,"register.html",{"form":form})
+    return render(request,"register.tmpl",{"form":form})
+
+def register_success(request):
+    return render(request,"register_success.tmpl")
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=20)
-    password = forms.CharField(widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(max_length=20,min_length=6)
+    password0 = forms.CharField(widget=forms.PasswordInput(),min_length=6)
+    password1 = forms.CharField(widget=forms.PasswordInput(),min_length=6)
+
+    def clean_password1(self):
+        password0 = self.cleaned_data['password0']
+        password1 = self.cleaned_data['password1']
+        
+        if password0 != password1:
+            del self.cleaned_data['password0']
+            del self.cleaned_data['password1']
+            raise forms.ValidationError("ValidationError")
+        
+        return password0
