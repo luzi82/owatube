c3ab385710d4c9fc7c01abe7dae498844d521b7f
luzi82 <luzi82@gmail.com>
2012-08-14 17:46:24 +0800

    add some dummy func

--- game/views.py
+++ game/views.py
@@ -2,3 +2,21 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Game index")
+
+def get_game_list(request):
+    return HttpResponse("dummy")
+
+def get_game(request):
+    return HttpResponse("dummy")
+
+def add_game_comment(request):
+    return HttpResponse("dummy")
+
+def add_game_score(request):
+    return HttpResponse("dummy")
+
+def add_game(request):
+    return HttpResponse("dummy")
+
+def edit_game(request):
+    return HttpResponse("dummy")
