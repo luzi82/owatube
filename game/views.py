f7c39f83eb48dd4eff90ce556c03ee0287038ac5
luzi82 <luzi82@gmail.com>
2012-08-15 17:59:04 +0800

    done get_game_list_fw

--- game/views.py
+++ game/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
 
 def index(request):
     return render(request,"index.tmpl")
 
 def get_game_list_fw(request):
-    return render(request,"dummy.tmpl")
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse("game.views.index"))
+    return HttpResponseRedirect(reverse("game.views.get_game_list",kwargs={"username":request.user.username}))
 
 def get_game_list(request, username):
     return render(request,"dummy.tmpl")
