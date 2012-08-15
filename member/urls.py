6d0c88e2a60a0e3bfcd496a5a4204860269a6fd1
luzi82 <luzi82@gmail.com>
2012-08-15 14:39:41 +0800

    login, logout, base html

--- member/urls.py
+++ member/urls.py
@@ -4,3 +4,8 @@ urlpatterns = patterns("member.views",
     url(r'^register/$', 'register'),
     url(r'^register_success/$', 'register_success'),
 )
+
+urlpatterns += patterns("django.contrib.auth.views",
+    url(r'^login/$', 'login', {'template_name': 'login.tmpl'}),
+    url(r'^logout/$', 'logout', {'template_name': 'logout.tmpl'}),
+)
