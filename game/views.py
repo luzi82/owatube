ae6baebd782731fb3a8a448f9e9c2aabf1754be8
luzi82 <luzi82@gmail.com>
2012-08-15 14:58:10 +0800

    add view param

--- game/views.py
+++ game/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 def index(request):
     return render(request,"index.tmpl")
 
-def get_game_list(request):
+def get_game_list(request, username):
     return HttpResponse("dummy")
 
-def get_game(request):
+def get_game(request, game_id):
     return HttpResponse("dummy")
 
 def add_game_comment(request):
@@ -19,5 +19,5 @@ def add_game_score(request):
 def add_game(request):
     return HttpResponse("dummy")
 
-def edit_game(request):
+def edit_game(request, game_id):
     return HttpResponse("dummy")
