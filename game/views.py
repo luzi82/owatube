adc4e2062cb9f95047bf4a336feb078c2a3823bc
luzi82 <luzi82@gmail.com>
2012-08-26 13:49:20 +0800

    remove comment

--- game/views.py
+++ game/views.py
@@ -28,17 +28,6 @@ def _check_game_entry(f):
     def ff(request,game_entry,*args,**kwargs):
         game = Game.objects.get(pk = game_entry)
         return f(request,game)
-#        username=request.GET.get("u",None)
-#        if username!=None:
-#            try:
-#                user = User.objects.get(username__exact = username)
-#                return f(request,*args,user=user,**kwargs)
-#            except User.DoesNotExist:
-#                return HttpResponseRedirect(reverse("game.views.index"))
-#        if not request.user.is_authenticated():
-#            return HttpResponseRedirect(reverse("game.views.index"))
-#        p = {"u":request.user.username}
-#        return HttpResponseRedirect(request.path+"?"+urllib.urlencode(p))
     return ff
 
 # view func
