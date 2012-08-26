21902c1734cc5074ca0e901565914eefdf096326
luzi82 <luzi82@gmail.com>
2012-08-26 12:36:32 +0800

    change _check_u structure

--- game/views.py
+++ game/views.py
@@ -5,15 +5,21 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from game.models import Game
 import urllib
+from django.contrib.auth.models import User
 
 ## helper function
 
 def _check_u(f):
     def ff(request,*args,**kwargs):
-        username=_get_user(request)
-        if username!=None:return f(request,*args,username=username,**kwargs)
+        username=request.GET.get("u",None)
+        if username!=None:
+            try:
+                user = User.objects.get(username__exact = username)
+                return f(request,*args,user=user,**kwargs)
+            except User.DoesNotExist:
+                return HttpResponseRedirect(reverse("game.views.index"))
         if not request.user.is_authenticated():
-            HttpResponseRedirect(reverse("game.views.index"))
+            return HttpResponseRedirect(reverse("game.views.index"))
         p = {"u":request.user.username}
         return HttpResponseRedirect(request.path+"?"+urllib.urlencode(p))
     return ff
@@ -24,15 +30,17 @@ def index(request):
     return render(request,"game/index.tmpl")
 
 @_check_u
-def get_user_profile(request,username):
-    return render(request,"dummy.tmpl",{"msg":username})
-
-@_check_u
-def get_game_score_list(request, username):
+def get_user_profile(request,user):
+#    game_list = Game.objects.filter()
+#    return render(request,"dummy.tmpl",{"game_list":game_list})
     return render(request,"dummy.tmpl")
 
 @_check_u
-def get_game_list(request, username):
+def get_game_score_list(request, user):
+    return render(request,"dummy.tmpl")
+
+@_check_u
+def get_game_list(request, user):
     return render(request,"dummy.tmpl")
 
 def get_game(request, game_entry):
@@ -72,8 +80,3 @@ class AddGameForm (forms.Form):
 def edit_game(request, game_entry):
     return render(request,"dummy.tmpl")
 
-
-######
-
-def _get_user(request):
-    return request.GET.get("u",None)
