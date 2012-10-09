ef3fea205d888b1eb4a2109b4e9cbaa9c7ab9ec6
luzi82 <luzi82@gmail.com>
2012-10-09 17:08:04 +0800

    game_edit, url change

--- game/urls.py
+++ game/urls.py
@@ -8,7 +8,4 @@ urlpatterns = patterns('game.views',
     url(r'^comment/$', 'add_game_comment'),
     url(r'^upload/$', 'add_game'),
     url(r'^submit/$', 'submit_game'),
-    url(r'^edit/(?P<game_entry>[0-9A-Za-z]+)/$', 'edit_game'),
-    url(r'^edit/(?P<game_entry>[0-9A-Za-z]+)/data.txt$', 'get_game_data'),
-    url(r'^edit/(?P<game_entry>[0-9A-Za-z]+)/bgm.mp3$',  'get_game_bgm'),
 )
--- game/urls_g.py
+++ game/urls_g.py
@@ -2,8 +2,13 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('game.views',
     url(r'^$', 'index'),
+
     url(r'^(?P<game_entry>[0-9A-Za-z]+)/$', 'get_game'),
     url(r'^(?P<game_entry>[0-9A-Za-z]+)/data.txt$', 'get_game_data'),
     url(r'^(?P<game_entry>[0-9A-Za-z]+)/bgm.mp3$', 'get_game_bgm'),
     url(r'^(?P<game_entry>[0-9A-Za-z]+)/owata.swf$', 'get_game_swf'),
+
+    url(r'^(?P<game_entry>[0-9A-Za-z]+)/edit/$', 'edit_game'),
+    url(r'^(?P<game_entry>[0-9A-Za-z]+)/edit/data.txt$', 'get_game_data'),
+    url(r'^(?P<game_entry>[0-9A-Za-z]+)/edit/bgm.mp3$',  'get_game_bgm'),
 )
